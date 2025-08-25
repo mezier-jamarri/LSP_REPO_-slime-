1299702b8f804180f469491772753f0a26a2aaa6
mezier-jamarri <mezier.jamarri@gmail.com>
2025-08-25 17:40:17 -0400

    assignment 1: hello world

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Name: <Jamarri Mezier>
+ */
+package org.howard.edu.lsp.assignment1;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
